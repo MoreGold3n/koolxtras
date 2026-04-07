ebe6bbac5602dfc7650feb9d9e98bbd1e9bd01a1
stav <126665568+sstvskids@users.noreply.github.com>
2026-04-07 11:25:42 +1000

    Create Entity.lua

---
+++ libraries/bridgeduel/Entity.lua
@@ -0,0 +1,36 @@
+local cloneref = cloneref or function(obj)
+	return obj
+end
+
+local Services = setmetatable({}, {
+	__index = function(self, obj)
+		return cloneref(game:GetService(obj))
+	end
+})
+
+local ReplicatedStorage = Services.ReplicatedStorage
+local Players = Services.Players
+local lplr = Players.LocalPlayer
+
+local Entity = {
+    FindByPlayer = function(plr)
+        for _, v in ReplicatedStorage.Modules.Knit.Services.EntityService.RF.GetEntities:InvokeServer() do
+            if v.Player == plr then
+                return v
+            end
+        end
+        
+        return nil
+    end,
+    FindByCharacter = function(char)
+        for _, v in ReplicatedStorage.Modules.Knit.Services.EntityService.RF.GetEntities:InvokeServer() do
+            if v.Character == char then
+                return v
+            end
+        end
+        
+        return nil
+    end
+}
+
+Entity.LocalEntity = Entity.FindByPlayer(lplr)
