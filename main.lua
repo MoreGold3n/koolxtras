335e7ddd359906dbbb227cd497f39b0a82c1b96a
stav <126665568+sstvskids@users.noreply.github.com>
2026-04-13 13:25:01 +1000

    Update main.lua

--- main.lua
+++ main.lua
@@ -30,18 +30,6 @@ local function downloadFile(file)
     return readfile(file)
 end
 
-if identifyexecutor then
-	if string.find(string.lower(({identifyexecutor()})[1]), 'jjsploit') or string.find(string.lower(({identifyexecutor()})[1]), 'bytebreaker') then
-		getgenv().identifyexecutor = function()
-			return 'Xeno'
-		end
-	end
-
-	if table.find({'Xeno', '5.0'}, ({identifyexecutor()})[1]) or not (debug.getupvalue or debug.getupvalues or debug.getproto or debug.getconstants or hookfunction or hookmetamethod or getconnections or require) then
-		shared.badexecs = true
-	end
-end
-
 for i,v in ids do
     if table.find(v, game.PlaceId) then
         local suc, res = pcall(downloadFile, 'koolaid/games/'..i..'.lua')
